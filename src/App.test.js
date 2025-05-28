20b135bda11d1954127211c00c7f104a96d3d5c7
syim <syim95@gtsoft.co.kr>
2025-05-28 17:13:50 +0900

    test 부분 주석

--- src/App.test.js
+++ src/App.test.js
@@ -1,11 +1,13 @@
 import { fireEvent, render, screen } from "@testing-library/react";
 import App from "./App";
+import axios from "axios";
+import ChartBit from "./component/ChartBit";
 
-test("the app has ChartBit", () => {
+/* test("the app has ChartBit", () => {
   render(<App />);
   const counterElement = screen.getByTestId("App");
   expect(counterElement).tobeInTheDocument();
-});
+}); */
 
 /* test("the counter starts at 0", () => {
 render(<App />);
