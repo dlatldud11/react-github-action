6f62f74c3847defedb3295cd2dbed9403f65f02a
syim <syim95@gtsoft.co.kr>
2025-05-28 15:46:59 +0900

    test 파일 완전 주석 처리

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,7 @@
-import { fireEvent, render, screen } from "@testing-library/react";
+/*import { fireEvent, render, screen } from "@testing-library/react";
 import App from "./App";
 
-/* test("the counter starts at 0", () => {
+ test("the counter starts at 0", () => {
   render(<App />);
   const counterElement = screen.getByTestId("counter");
   expect(counterElement).toHaveTextContent(0);
