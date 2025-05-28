e11e9d0b1727d8ff5fd229b3bfbc32fd724e952f
syim <syim95@gtsoft.co.kr>
2025-05-28 15:45:08 +0900

    안쓰이는 변수 정리

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
 import "./App.css";
-import { useState } from "react";
-import Chart from "./component/ChartBit";
 import ChartBit from "./component/ChartBit";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <div className="App">
       <ChartBit />
