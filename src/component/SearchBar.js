9e5d4987b7b8800286b652fcffd9a52bd2760d8d
syim <syim95@gtsoft.co.kr>
2025-06-13 09:47:29 +0900

    css수정

--- src/component/SearchBar.js
+++ src/component/SearchBar.js
@@ -12,30 +12,39 @@ export default function SearchBar({
 }) {
   return (
     <>
-      <div className="row">
+      <div className="chart-header">
         <h3>{market} MACD 차트</h3>
       </div>
-      <div className="row">
-        <select onChange={(e) => setMarket(e.target.value)} value={market}>
-          {markets.length > 0 &&
-            markets.map((i) => (
-              <option key={i.market} value={i.market}>
-                {i.korean_name}
+      <div className="chart-controls">
+        <div className="control-group">
+          <label htmlFor="market">코인</label>
+          <select onChange={(e) => setMarket(e.target.value)} value={market}>
+            {markets.length > 0 &&
+              markets.map((i) => (
+                <option key={i.market} value={i.market}>
+                  {i.market} {i.korean_name}
+                </option>
+              ))}
+          </select>
+        </div>
+        <div className="control-group">
+          <label htmlFor="date">날짜</label>
+          <input
+            type="date"
+            value={date || ""}
+            onChange={(e) => setDate(e.target.value)}
+          />
+        </div>
+        <div className="control-group">
+          <label htmlFor="minute">분봉</label>
+          <select onChange={(e) => setMinute(e.target.value)} value={minute}>
+            {minuteOptions.map((i) => (
+              <option key={i.key} value={i.value}>
+                {i.value} 분봉
               </option>
             ))}
-        </select>
-        <input
-          type="date"
-          value={date || ""}
-          onChange={(e) => setDate(e.target.value)}
-        />
-        <select onChange={(e) => setMinute(e.target.value)} value={minute}>
-          {minuteOptions.map((i) => (
-            <option key={i.key} value={i.value}>
-              {i.value} 분봉
-            </option>
-          ))}
-        </select>
+          </select>
+        </div>
       </div>
     </>
   );
